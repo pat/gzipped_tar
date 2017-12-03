2df53045950218e7e470144d8b653417fb7956e4
Pat Allan <pat@freelancing-gods.com>
2017-12-03 23:26:53 +1100

    The reader

---
+++ lib/gzipped_tar/reader.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rubygems/package"
+
+module GZippedTar
+  class Reader
+    def initialize(raw)
+      @raw = raw
+    end
+
+    def read(path)
+      result = reader.detect { |entry| entry.full_name == path }
+      result && result.read
+    ensure
+      reader.rewind
+    end
+
+    private
+
+    attr_reader :raw
+
+    def reader
+      @reader ||= Gem::Package::TarReader.new unzipped
+    end
+
+    def unzipped
+      Zlib::GzipReader.new StringIO.new(raw, "r+b")
+    end
+  end
+end
---
+++ spec/gzipped_tar/reader_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "gzipped_tar/reader"
+require "tmpdir"
+
+RSpec.describe GZippedTar::Reader do
+  let(:subject)      { GZippedTar::Reader.new File.read(archive_path) }
+  let(:archive_path) { "archive.tar.gz" }
+
+  around :example do |example|
+    Dir.mktmpdir do |directory|
+      Dir.chdir(directory) { example.run }
+    end
+  end
+
+  before :example do
+    File.write "file.txt", "hello world"
+
+    `tar -czf #{archive_path} file.txt`
+  end
+
+  describe "#read" do
+    it "returns the contents of the file at the given path" do
+      expect(subject.read("file.txt")).to eq("hello world")
+    end
+
+    it "returns nil if the path is not valid" do
+      expect(subject.read("missing.ext")).to be_nil
+    end
+
+    it "can find files more than once" do
+      expect(subject.read("file.txt")).to eq("hello world")
+      expect(subject.read("file.txt")).to eq("hello world")
+    end
+  end
+end
